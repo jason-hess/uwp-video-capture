6547e3f33060760d07b11b2e2861337b2cbd4071
Jason Hess <Jason.Hess@michaelhill.com.au>
2018-12-31 09:54:37 +1000

    refactor

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace App1
+﻿namespace App1
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
