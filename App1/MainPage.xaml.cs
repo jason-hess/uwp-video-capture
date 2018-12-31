8133a351643c8ce2a61a966fb8e5c566366bb026
Jason Hess <Jason.Hess@michaelhill.com.au>
2018-12-31 10:28:55 +1000

    add speech

--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace App1
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace App1
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -8,6 +12,15 @@
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, I am mj.  Your personal AI.");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
         }
     }
 }
