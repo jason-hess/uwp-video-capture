52bb0646055350fbeb7661cf3ced9d4400a4f3ad
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-01-21 20:06:52 +1000

    add speech recognition

---
+++ App1/AudioCapturePermissions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Media.Capture;
+
+namespace App1
+{
+    public class AudioCapturePermissions
+    {
+        // If no microphone is present, an exception is thrown with the following HResult value.
+        private static int NoCaptureDevicesHResult = -1072845856;
+
+        /// <summary>
+        /// Note that this method only checks the Settings->Privacy->Microphone setting, it does not handle
+        /// the Cortana/Dictation privacy check.
+        ///
+        /// You should perform this check every time the app gets focus, in case the user has changed
+        /// the setting while the app was suspended or not in focus.
+        /// </summary>
+        /// <returns>True, if the microphone is available.</returns>
+        public static async Task<bool> RequestMicrophonePermission()
+        {
+            try
+            {
+                // Request access to the audio capture device.
+                var settings = new MediaCaptureInitializationSettings
+                {
+                    StreamingCaptureMode = StreamingCaptureMode.Audio, MediaCategory = MediaCategory.Speech
+                };
+                var capture = new MediaCapture();
+
+                await capture.InitializeAsync(settings);
+            }
+            catch (TypeLoadException)
+            {
+                // Thrown when a media player is not available.
+                var messageDialog = new Windows.UI.Popups.MessageDialog("Media player components are unavailable.");
+                await messageDialog.ShowAsync();
+                return false;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Thrown when permission to use the audio capture device is denied.
+                // If this occurs, show an error or disable recognition functionality.
+                return false;
+            }
+            catch (Exception exception)
+            {
+                // Thrown when an audio capture device is not present.
+                if (exception.HResult == NoCaptureDevicesHResult)
+                {
+                    var messageDialog = new Windows.UI.Popups.MessageDialog("No Audio Capture devices are present on this system.");
+                    await messageDialog.ShowAsync();
+                    return false;
+                }
+
+                throw;
+            }
+            return true;
+        }
+    }
+}
--- App1/MainPage.xaml.cs
+++ App1/MainPage.xaml.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Windows.Media.SpeechSynthesis;
-using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -22,12 +21,14 @@ namespace App1
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            await SayWithTheVoice("Hello, I am MJ, your personal AI.  You are looking fine today.  How can I help?", "James");
+            await SayWithTheVoice(
+                $"Hello {txtName.Text}! I am MJ, your personal AI.  You're looking quite dashing today!  How can I help?",
+                "James");
         }
 
-        private static async Task SayWithTheVoice(string text, string speaker)
+        private async Task SayWithTheVoice(string text, string speaker)
         {
-            var mediaElement = new MediaElement();
+            var mediaElement = medElement;
             using (var synth = new SpeechSynthesizer())
             {
                 var voices = SpeechSynthesizer.AllVoices.ToArray();
@@ -38,14 +39,72 @@ namespace App1
                 var stream = await synth.SynthesizeTextToStreamAsync(text);
                 mediaElement.SetSource(stream, stream.ContentType);
 
+                var tsc = new TaskCompletionSource<bool>();
+                mediaElement.MediaEnded += (o, e) => { tsc.TrySetResult(true); };
                 mediaElement.Play();
+                await tsc.Task;
             }
         }
 
         private async void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            await SayWithTheVoice("Hello, I am Sam, The Tip of the Sword, and the better looking AI.  You're looking fine today.  How can I help?", "Mark");
+            await SayWithTheVoice(
+                $"Hello {txtName.Text}, I am Sam, The Tip of the Sword, and the better looking AI.  You're looking fine today.  How can I help?",
+                "Mark");
 
+
+            // Create an instance of SpeechRecognizer.
+            var speechRecognizer = new Windows.Media.SpeechRecognition.SpeechRecognizer();
+
+            // Compile the dictation grammar by default.
+            await speechRecognizer.CompileConstraintsAsync();
+
+            // Start recognition.
+            Windows.Media.SpeechRecognition.SpeechRecognitionResult speechRecognitionResult = await speechRecognizer.RecognizeAsync();
+
+            // Do something with the recognition result.
+            //var messageDialog = new Windows.UI.Popups.MessageDialog(speechRecognitionResult.Text, "Text spoken");
+            //await messageDialog.ShowAsync();
+
+            if (speechRecognitionResult.Text.Contains("coffee"))
+            {
+                await SayWithTheVoice("I'm sorry, I don't make coffee", "Mark");
+            }
+            else if (speechRecognitionResult.Text.Contains("chocolate"))
+            {
+                await SayWithTheVoice("Coming right up!", "Mark");
+            }
+            else
+            {
+                await SayWithTheVoice("I'm confused", "Mark");
+            }
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            await SayWithTheVoice(
+                $"{txtName.Text}",
+                "Mark");
+
+        }
+
+        private async void MainPage_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (!await AudioCapturePermissions.RequestMicrophonePermission())
+            {
+                return;
+            }
+
+        }
+
+        private async void Button_Click_3(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
